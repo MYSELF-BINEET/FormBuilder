77fd9543714dae5ba4588eee114f403508560e5f
BINEET PRADHAN <121740189+MYSELF-BINEET@users.noreply.github.com>
2024-12-13 08:50:36 +0530

    Update constants.ts

--- client/src/utils/constants.ts
+++ client/src/utils/constants.ts
@@ -1,4 +1,4 @@
-export const BACKEND_BASE_URL = "https://formbuilder-2-15ix.onrender.com";
+export const BACKEND_BASE_URL = "https://formbuilder-2-15ix.onrender.com/api/v1";
 export const SECRET_KEY = "7747116a99a43eec8bde0ec1d01234c63e61d347b1f2b6051d29332afaab2285487a0aaee740aa638a73490793e5750a918e173b9afe3ba0589bc065702bbe84";
 
 export const cookieMaxAge = 60 * 60 * 24 * 7; // 7 days
